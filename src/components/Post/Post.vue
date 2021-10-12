401e5eb93749712bb7d409589c28160e4a37eca6
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2021-10-11 23:32:20 -0400

    post updates

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -5,20 +5,6 @@
 
     <nuxt-content class="post__article my-8" :document="article" />
 
-    <div class="post__date">
-      <h3>Post Date:</h3>
-      <div>{{ articleDate }}</div>
-    </div>
-
-    <div class="post__tags">
-      <h3>Tagged:</h3>
-      <ul class="post__tag-list">
-        <li v-for="tag in article.tags" :key="tag" class="post__tag-item">
-          <nuxt-link :to="`/tagged/${tag}`">{{ tag }}</nuxt-link>
-        </li>
-      </ul>
-    </div>
-
   </section>
 </template>
 

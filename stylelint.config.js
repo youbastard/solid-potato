9013c59b22eb746df666e3564feaf817a378df82
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2021-10-11 23:32:09 -0400

    update stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,7 @@ module.exports = {
   ],
   rules: {
     'at-rule-empty-line-before': null,
+    'block-no-empty': null,
     'comment-empty-line-before': null,
     'custom-property-empty-line-before': null,
     'rule-empty-line-before': null

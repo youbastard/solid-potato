ccd29781984b3d279ebace78d6d07e2434aa2e38
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2021-10-11 23:11:54 -0400

    remove theme script

--- static/scripts/theme.js
+++
@@ -1,7 +0,0 @@
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.querySelector('html').setAttribute('data-theme-dark', true);
-  localStorage.theme = 'dark';
-} else {
-  document.querySelector('html').removeAttribute('data-theme-dark');
-  localStorage.theme = 'light';
-}

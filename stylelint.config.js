ebff185fa3b9cc62d30850d9b50a7cdf53001786
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2021-10-11 23:12:46 -0400

    new stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,10 +2,10 @@ module.exports = {
   extends: [
     'stylelint-config-standard'
   ],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
   rules: {
-    'rule-empty-line-before': null,
-    'at-rule-empty-line-before': null
+    'at-rule-empty-line-before': null,
+    'comment-empty-line-before': null,
+    'custom-property-empty-line-before': null,
+    'rule-empty-line-before': null
   }
 };

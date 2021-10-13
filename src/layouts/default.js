5d248f73a4555ac8287d2b63981a31ec30624495
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2021-10-13 12:49:39 -0400

    remove sfc for default. add head info

---
+++ src/layouts/default.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'Layout',
+  head: {
+    title: 'hello sawtooth',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  }
+};
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,28 +7,5 @@
   </div>
 </template>
 
-<style scoped>
-.layout {
-  display: flex;
-  flex-direction: column;
-  background-color: var(--bg-primary);
-  color: var(--text-primary);
-  font-family: var(--font-base);
-}
-
-.layout__header,
-.layout__footer {
-  background-color: var(--bg-secondary);
-}
-
-.layout__main {
-  margin: 0 auto;
-  flex-grow: 1;
-}
-
-.layout__title {
-  font-size: 2rem;
-  letter-spacing: 1px;
-  text-transform: lowercase;
-}
-</style>
+<script src="./default.js" />
+<style src="./default.scss" lang="scss" />

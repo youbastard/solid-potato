b9cc4765d956d2d80fc596f44d34b880c91476ae
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2021-10-16 23:04:51 -0400

    edit post page to use post component

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -1,12 +1,10 @@
 <template>
   <section class="post">
-
-    <h1 class="post__title my-8">{{ article.title }}</h1>
-
-    <nuxt-content class="post__article my-8" :document="article" />
-
+    <h1 class="post__title">{{ article.title }}</h1>
+    <nuxt-content class="post__article" :document="article" />
+    <a :href="article.url"> Read more about {{ article.title }} on Wikipedia</a>
   </section>
 </template>
 
 <script src="./Post.js" />
-<style src="./Post.css" />
+<style src="./Post.scss" lang="scss" />
--- src/pages/post/_slug.vue
+++ src/pages/post/_slug.vue
@@ -1,10 +1,6 @@
 <template>
   <main class="post">
-
-    <h1 class="post__title my-8">{{ article.title }}</h1>
-
-    <nuxt-content class="post__article my-8" :document="article" />
-
+    <post :article="article" />
   </main>
 </template>
 

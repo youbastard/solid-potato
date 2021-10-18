56b0d926345f49deaf1d3001a3b80eb6fd557970
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2021-10-18 11:56:41 -0400

    add style for more link

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -2,7 +2,7 @@
   <section class="post">
     <h1 class="post__title">{{ article.title }}</h1>
     <nuxt-content class="post__article" :document="article" />
-    <a :href="article.url"> Read more about {{ article.title }} on Wikipedia</a>
+    <a class="post__read-more" rel="external" :href="article.url"> Read more about {{ article.title }} on Wikipedia</a>
   </section>
 </template>
 
